3dec505b3d932180480b6937bbfb1f71dd38bde1
Andrey Smirnov <mail@ansmirnov.ru>
2018-03-05 13:03:03 +0300

    Add Russian keyboard

--- init.el
+++ init.el
@@ -1,6 +1,28 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'ob-tangle)
 ;; (setq debug-on-error t)
 (org-babel-load-file
    (expand-file-name "README.org" user-emacs-directory))
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ob-ipython markdown-mode php-mode ein elpy flycheck ssh magit reverse-im company ivy google-translate zenburn-theme use-package)))
+ '(reverse-im-input-methods (quote ("russian-computer"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
