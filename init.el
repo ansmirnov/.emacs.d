4975085bc1c57eda45d7212ad25240780b2c5246
Andrey Smirnov <mail@ansmirnov.ru>
2018-03-13 15:42:50 +0300

    Add custom file.
    Add some display features.

--- init.el
+++ init.el
@@ -11,20 +11,3 @@
    (expand-file-name "README.org" user-emacs-directory))
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
- '(package-selected-packages
-   (quote
-    (emms material-theme ob-ipython markdown-mode php-mode ein elpy flycheck ssh magit reverse-im company ivy google-translate zenburn-theme use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
